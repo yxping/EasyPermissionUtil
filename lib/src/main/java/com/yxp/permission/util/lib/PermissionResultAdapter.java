1ba920354801de8b33b13cbca3e0678f0a8a955e
yanxing <846922168@qq.com>
2016-01-16 17:38:35 +0800

    refresh PermissionResultAdapter

--- lib/src/main/java/com/yxp/permission/util/lib/PermissionResultAdapter.java
+++ lib/src/main/java/com/yxp/permission/util/lib/PermissionResultAdapter.java
@@ -9,24 +9,4 @@ import java.util.Map;
  * Created by yanxing on 16/1/15.
  */
 public abstract class PermissionResultAdapter implements PermissionResultCallBack {
-
-    @Override
-    public void onPermissionGranted() {
-
-    }
-
-    @Override
-    public void onPermissionDenied(String... permissions) {
-
-    }
-
-    @Override
-    public void onRationalShow(String... permissions) {
-
-    }
-
-    @Override
-    public void onResult(Map<String, List<PermissionInfo>> result) {
-
-    }
 }
